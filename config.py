e95bdfbfe1af4d0dfc6fb8c7fa411eabc31a09b1
Thesmo111 <85233428+Thesmo111@users.noreply.github.com>
2024-01-24 16:30:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-discord_bot_token = "MTE2MjM2NjgzNTE2MTgzMzQ4Mg.GrW6nI.dSKOppn7FG-KF0LJN-X9fCEh5imKQr5MxWxMV4"
-openai_api_key = "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"
+discord_bot_token = "---"
+openai_api_key = "---"
